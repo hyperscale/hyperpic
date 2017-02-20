c1646b75f15b32344ee209c215614462ae13c58b
Axel Etcheverry <aetcheverry@molotov.tv>
2017-02-20 13:22:58 +0100

    feat(project): Added header comment

--- color.go
+++ color.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Axel Etcheverry. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 package main
 
 var colorsToRGB = map[string][]uint8{
